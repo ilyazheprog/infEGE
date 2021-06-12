c07e540fea8b5e71c2dfd52e2ceff704b3f92786
Ilya484 <ucdo854@kemcdo.ru>
2021-06-12 18:23:38 +0700

    .gitignore update

--- for_testing_and_debug.py
+++ for_testing_and_debug.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from modul import *
-a, b = read(int, int)
+from modul import Translation_systems_counts as tr
+operations = '^∨¬⊕⇒'
+print('Операции для логических операций(копируйте их):', operations)
+s = 'x '
+print(tr(7715, 8, 2))
