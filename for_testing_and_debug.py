f21d79217e7e0d4e8a2ee24ed06a36fb74d74b1d
Ilya484 <ucdo854@kemcdo.ru>
2021-06-12 18:41:07 +0700

    del filees

--- for_testing_and_debug.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from modul import Translation_systems_counts as tr
-operations = '^∨¬⊕⇒'
-print('Операции для логических операций(копируйте их):', operations)
-s = 'x '
-print(tr(7715, 8, 2))
