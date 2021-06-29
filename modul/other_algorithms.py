d0aebf8ecaac2af2f57adc2e304b681aaddd1e54
Ilya484 <ucdo854@kemcdo.ru>
2021-06-29 23:48:57 +0700

    добавил комбинаторные функции(перестановки, перестановки с повторениями) и функцию построения таблицы истинности лог.функций

--- modul/other_algorithms.py
+++ modul/other_algorithms.py
@@ -1,3 +1,75 @@
+def new_num_sys(number: object, old_base: int = 10, new_base: int = 10) -> object:
+    """
+    Переводит число number с основанием old_base в
+    число с основанием new_base
+    :param number:
+    :param old_base:
+    :param new_base:
+    :return: object
+    """
+    if old_base == new_base:
+        return number
+    elif not (2 < old_base < 36) and not (2 < new_base < 36):
+        return None
+    elif new_base == 10:
+        return int(str(number), old_base)
+    elif old_base == 10:
+        mods = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        r = ''
+        while number:
+            number, y = divmod(number, new_base)
+            r = mods[y] + r
+        return r
+    else:
+        return new_num_sys(new_num_sys(number, old_base), 10, new_base)
+
+
+def permutation_repeat(vars, k):
+    masks=[]
+    res=[]
+    for v in range(len(vars)**k):
+        n=new_num_sys(v, new_base=len(vars))
+        masks+=['0'*(k-len(n))+n]
+    if type(vars)==str:
+        for m in masks:
+            s=''
+            for i in m:
+                s+=' '+vars[int(i)]
+            res.append(tuple(map(str, s.split())))
+        
+    if type(vars)==list:
+        for m in masks:
+            s=[]
+            for i in m:
+                s+=[vars[int(i)]]
+            res.append(tuple(s))
+    return tuple(res)
+
+def permutations(lst):
+    if len(lst) == 0:
+        return []
+    if len(lst) == 1:
+        return [lst]
+    l = []
+    for i in range(len(lst)):
+        m = lst[i]
+        remLst = lst[:i] + lst[i + 1:]
+        for p in permutations(remLst):
+            l.append([m] + p)
+    return l
+
+def printTrueTable(vars, expretion, value='all'):
+    d = permutation_repeat([0, 1], len(vars))
+    print(vars, 'F')
+    for el in d:
+        copyexp = expretion
+        for varib in vars:
+            copyexp = copyexp.replace(varib, str(el[vars.index(varib)]))
+        if int(eval(copyexp))==value:
+            print(*el, ' ', value, sep='')
+        if value=='all':
+            print(*el, ' ', int(eval(copyexp)), sep='')
+
 def data_tr(n: float, old: str, new: str) -> float:
     """
     Переводит объёмы информации
@@ -51,27 +123,4 @@ def count(substring: str, string: str) -> int:
     return count
 
 
-def new_num_sys(number: object, old_base: int = 10, new_base: int = 10) -> object:
-    """
-    Переводит число number с основанием old_base в
-    число с основанием new_base
-    :param number:
-    :param old_base:
-    :param new_base:
-    :return: object
-    """
-    if old_base == new_base:
-        return number
-    elif not (2 < old_base < 36) and not (2 < new_base < 36):
-        return None
-    elif new_base == 10:
-        return int(str(number), old_base)
-    elif old_base == 10:
-        mods = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-        r = ''
-        while number:
-            number, y = divmod(number, new_base)
-            r = mods[y] + r
-        return r
-    else:
-        return new_num_sys(new_num_sys(number, old_base), 10, new_base)
+
